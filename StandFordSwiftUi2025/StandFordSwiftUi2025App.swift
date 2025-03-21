38085927e1189a3794eb38b05bf14156125425e4
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-03-21 12:07:40 +0100

    Initial Commit

---
+++ StandFordSwiftUi2025/StandFordSwiftUi2025App.swift
@@ -0,0 +1,17 @@
+//
+//  StandFordSwiftUi2025App.swift
+//  StandFordSwiftUi2025
+//
+//  Created by Авазбек Надырбек уулу on 21.03.25.
+//
+
+import SwiftUI
+
+@main
+struct StandFordSwiftUi2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
